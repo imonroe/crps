b0c81d502c39dde9bd2cb8df601def92b226a57b
Ian Monroe <ian@ianmonroe.com>
2017-10-26 17:54:53 -0500

    adding initial support for per-user subjects and aspects

---
+++ src/migrations/2017_10_26_221827_AddUserfieldsForCrps.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserfieldsForCrps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+      if (Schema::hasTable('aspects')) {
+        Schema::table('aspects', function (Blueprint $table) {
+            $table->integer('user');
+        });
+      }
+
+      if (Schema::hasTable('subjects')) {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->integer('user');
+        });
+      }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+      Schema::table('aspects', function (Blueprint $table) {
+        $table->dropColumn('user');
+      });
+
+      Schema::table('subjects', function (Blueprint $table) {
+        $table->dropColumn('user');
+      });
+
+    }
+}
