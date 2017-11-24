e30cfc29b5bf46e7ba31c6849b70e60c2ad982c1
Ian Monroe <ian@ianmonroe.com>
2017-11-24 12:57:36 -0600

    editable bug

--- src/Aspect.php
+++ src/Aspect.php
@@ -275,12 +275,7 @@ class Aspect extends Model implements HasMediaConversions
     }
 
     public function can_edit(){
-      dd($this);
-      if ($this->editable == 1){
-        return true;
-      } else {
-        return false;
-      }
+      return (bool)$this->editable;
     }
 
 } // End of base Aspect Class.
