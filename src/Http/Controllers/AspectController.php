8865e8f928e5a9bf004b7f201c8ed9e5eba0e060
Ian Monroe <ian@ianmonroe.com>
2017-11-26 15:41:40 -0600

    tinker

--- src/Http/Controllers/AspectController.php
+++ src/Http/Controllers/AspectController.php
@@ -254,7 +254,6 @@ class AspectController extends Controller
     public function destroy(Request $request, $id)
     {
         $garbage_aspect = Aspect::find($id);
-        dd($garbage_aspect);
         // fire the pre-delete hook, if available
         $garbage_aspect->pre_delete($request);
 
